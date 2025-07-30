54d8e0eab1b5c05c017b75efa1c6cacaf6068bee
HaoNP <nguyenphuochao123@gmail.com>
2025-07-30 22:32:32 +0700

    config DB server render

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connectDB() {
     try {
         // await mongoose.connect('mongodb://localhost:27017/note_management_dev');
-        await mongoose.connect('mongodb+srv://nguyenphuochao456:ZuvCPHE5KnQoZ9a4@cluster0.myinnsw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('Connect successfully');
     } catch (error) {
         console.log('Connect failed');
