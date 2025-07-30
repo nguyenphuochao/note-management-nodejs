5f65e1d2b840e4a519eefcff368e32d62ba54b73
HaoNP <nguyenphuochao123@gmail.com>
2025-07-30 22:01:29 +0700

    config DB deloy render server

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/note_management_dev');
+        // await mongoose.connect('mongodb://localhost:27017/note_management_dev');
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('Connect successfully');
     } catch (error) {
         console.log('Connect failed');
