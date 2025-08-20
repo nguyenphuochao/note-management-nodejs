2384fafe579ad6461b28db950c7644a53ec75c60
Nguyễn Phước Hảo <nguyenphuochao123@gmail.com>
2025-08-20 22:23:28 +0700

    refactor(notes): Integrate TinyMCE

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -90,6 +90,23 @@ document.addEventListener('DOMContentLoaded', function () {
             form.submit();
         }
     });
+
+    // Integrate TinyMCE
+    tinymce.init({
+        selector: 'textarea#description',
+        height: 500,
+        plugins: [
+            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+            'insertdatetime', 'media', 'table', 'help', 'wordcount'
+        ],
+        toolbar: 'undo redo | blocks | ' +
+            'bold italic backcolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }',
+        newline_behavior: 'linebreak',
+    });
 })
 
 
